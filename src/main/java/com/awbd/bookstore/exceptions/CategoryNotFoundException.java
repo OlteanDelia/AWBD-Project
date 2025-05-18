f6a02875f53b42dc93f182c9117c456ec84d555c
iulitaalpetri <iulitaalpetri66@gmail.com>
2025-05-18 20:36:58 +0300

    revise Category

--- src/main/java/com/awbd/bookstore/exceptions/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.awbd.bookstore.exceptions;
-
-public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException() {
-        super("The requested category was not found.");
-    }
-}
