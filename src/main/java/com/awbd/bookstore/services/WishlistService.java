fc1195210f7249955ba049a90b3304a3235d5f88
iulitaalpetri <iulitaalpetri66@gmail.com>
2025-05-19 00:10:34 +0300

    revise wishlist

--- src/main/java/com/awbd/bookstore/services/WishlistService.java
+++ src/main/java/com/awbd/bookstore/services/WishlistService.java
@@ -1,13 +1,15 @@
 package com.awbd.bookstore.services;
 
-
+import com.awbd.bookstore.exceptions.book.BookNotFoundException;
+import com.awbd.bookstore.exceptions.wishlist.BookAlreadyInWishlistException;
+import com.awbd.bookstore.exceptions.wishlist.WishlistNotFoundException;
+import com.awbd.bookstore.models.Book;
 import com.awbd.bookstore.models.Wishlist;
+import com.awbd.bookstore.repositories.BookRepository;
 import com.awbd.bookstore.repositories.WishlistRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.awbd.bookstore.models.Book;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -15,34 +17,60 @@ import java.util.Set;
 @Service
 @Transactional
 public class WishlistService {
-    private final WishlistRepository wishlistRepository;
+    private WishlistRepository wishlistRepository;
+    private BookRepository bookRepository;
 
-    public WishlistService(WishlistRepository wishlistRepository) {
+    public WishlistService(WishlistRepository wishlistRepository, BookRepository bookRepository) {
         this.wishlistRepository = wishlistRepository;
+        this.bookRepository = bookRepository;
     }
 
     public List<Book> getBooksByWishlistId(Long wishlistId) {
+        if (!wishlistRepository.existsById(wishlistId)) {
+            throw new WishlistNotFoundException("Wishlist with ID " + wishlistId + " not found");
+        }
         return wishlistRepository.findBooksByWishlistId(wishlistId);
     }
 
-    public Book addBookToWishlist(Long wishlistId, Long bookId ){
-        Optional<Wishlist> wishlistOptional = wishlistRepository.findById(wishlistId);
-        if (wishlistOptional.isPresent()) {
-            Wishlist wishlist = wishlistOptional.get();
-            Set<Book> books = wishlist.getBooks();
-            for (Book book : books) {
-                if (book.getId() == bookId) {
-                    throw new RuntimeException("Book already exists in the wishlist");
-                }
+    public Book addBookToWishlist(Long wishlistId, Long bookId) {
+        Wishlist wishlist = wishlistRepository.findById(wishlistId)
+                .orElseThrow(() -> new WishlistNotFoundException("Wishlist with ID " + wishlistId + " not found"));
+
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException("Book with ID " + bookId + " not found"));
+
+        Set<Book> books = wishlist.getBooks();
+        for (Book existingBook : books) {
+            if (existingBook.getId() == bookId) {
+                throw new BookAlreadyInWishlistException("Book already exists in the wishlist");
             }
-            Book newBook = new Book();
-            newBook.setId(bookId);
-            books.add(newBook);
-            wishlist.setBooks(books);
-            wishlistRepository.save(wishlist);
-            return newBook;
-        } else {
-            throw new RuntimeException("Wishlist not found");
         }
+
+        books.add(book);
+        wishlist.setBooks(books);
+        wishlistRepository.save(wishlist);
+
+        return book;
+    }
+
+    public void removeBookFromWishlist(Long wishlistId, Long bookId) {
+        Wishlist wishlist = wishlistRepository.findById(wishlistId)
+                .orElseThrow(() -> new WishlistNotFoundException("Wishlist with ID " + wishlistId + " not found"));
+
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException("Book with ID " + bookId + " not found"));
+
+        boolean removed = wishlist.getBooks().removeIf(b -> b.getId() == bookId);
+
+        if (!removed) {
+            throw new BookNotFoundException("Book with ID " + bookId + " not found in wishlist");
+        }
+
+        wishlistRepository.save(wishlist);
+    }
+
+    public Wishlist getWishlistById(Long wishlistId) {
+        return wishlistRepository.findById(wishlistId)
+                .orElseThrow(() -> new WishlistNotFoundException("Wishlist with ID " + wishlistId + " not found"));
     }
 }
