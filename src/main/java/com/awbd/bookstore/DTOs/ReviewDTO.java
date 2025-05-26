bce0990b11f6612dee51440baba00d192c35b002
iulitaalpetri <iulitaalpetri66@gmail.com>
2025-05-26 22:42:56 +0300

    resolve review bugs

--- src/main/java/com/awbd/bookstore/DTOs/ReviewDTO.java
+++ src/main/java/com/awbd/bookstore/DTOs/ReviewDTO.java
@@ -14,14 +14,14 @@ public class ReviewDTO {
     private Long id;
     private String content;
     private int rating;
-    private String createdAt;
+
     private Long userId;
     private Long bookId;
 
-    public ReviewDTO(String content, int rating, String createdAt, Long userId, Long bookId) {
+    public ReviewDTO(String content, int rating, Long userId, Long bookId) {
         this.content = content;
         this.rating = rating;
-        this.createdAt = createdAt;
+
         this.userId = userId;
         this.bookId = bookId;
     }
